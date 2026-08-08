c53cca1f04ce65920b4b10d454ebcfdc98c785c6
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2026-08-08 16:54:25 -0300

    feat: Resolução de Exercícios

--- Java/Aula02/Ex01.java
+++ Java/Aula02/Ex01.java
@@ -9,39 +9,44 @@ import java.util.ArrayList;
 public class Ex01 {
     static Scanner leia = new Scanner(System.in);
     static ArrayList<String> Jogos = new ArrayList<>();
+    static boolean Trava = true;
 
     public static void main(String[] args) {
 
         System.out.println("=== Bem vindo ===");
-        System.out.println("Selecione uma alternativa de 1 a 5.");
-        System.out.print(
-                "1 - Cadastrar Jogo\n2 - Listar Jogos\n3 - Atualizar Jogo\n4 - Remover Jogo\n5 - Sair\n\nOpção: ");
-        int userInt = leia.nextInt();
 
-        switch (userInt) {
-            case 1:
-                Cadastro();
-                break;
+        while (Trava) {
+            System.out.println("\nSelecione uma alternativa de 1 a 5.");
+            System.out.print(
+                    "1 - Cadastrar Jogo\n2 - Listar Jogos\n3 - Atualizar Jogo\n4 - Remover Jogo\n5 - Sair\n\nOpção: ");
+            int userInt = leia.nextInt();
 
-            case 2:
-                Listar();
-                break;
+            switch (userInt) {
+                case 1:
+                    Cadastro();
+                    break;
 
-            case 3:
-                Atualizar();
-                break;
+                case 2:
+                    Listar();
+                    break;
 
-            case 4:
-                Remover();
-                break;
+                case 3:
+                    Atualizar();
+                    break;
 
-            case 5:
-                Sair();
-                leia.close();
-                break;
+                case 4:
+                    Remover();
+                    break;
 
-            default:
-                break;
+                case 5:
+                    Sair();
+                    leia.close();
+                    break;
+
+                default:
+                    System.out.println("Opção inválida...");
+                    break;
+            }
         }
 
     }
@@ -58,7 +63,11 @@ public class Ex01 {
     }
 
     public static void Listar() {
+        System.out.println("> Lista de Jogos <");
 
+        for (int i = 0; i < Jogos.size(); i++) {
+            System.out.print((i + 1) + "º - " + Jogos.get(i) + "\n");
+        }
     }
 
     public static void Atualizar() {
@@ -70,7 +79,8 @@ public class Ex01 {
     }
 
     public static void Sair() {
-
+        System.out.println("Saindo...");
+        Trava = false;
     }
 
 }
