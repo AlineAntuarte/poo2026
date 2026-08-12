56080f64fa3cb3c12442e88217a8acbc11acf285
Aluno <aluno@sje-lin-mtrans-926174.maquinas.sj.ifsc.edu.br>
2026-08-11 21:43:44 -0300

    feat: Resolução de Exercícios

---
+++ Java/Aula03/Ex01.java
@@ -0,0 +1,13 @@
+package Java.Aula03;
+
+import java.util.Scanner;
+
+// Jogo da Forca
+public class Ex01 {
+
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        leia.close();
+    }
+}
