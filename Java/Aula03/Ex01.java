9ac18723dc805f0195d9a77c13ae60fbe0f6020c
Aluno <aluno@sje-lin-mtrans-926174.maquinas.sj.ifsc.edu.br>
2026-08-11 22:15:39 -0300

    feat: Resolução de Exercícios

--- Java/Aula03/Ex01.java
+++ Java/Aula03/Ex01.java
@@ -8,6 +8,20 @@ public class Ex01 {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
 
+        boolean Trava = true;
+        int tentativas = 5;
+
+        String[] listaDePalavras = { "Antuarte" };
+        String[] palavraOculta = {};
+
+        int letras = listaDePalavras[0].length();
+
+        for (int i = 0; i < letras; i++) {
+            System.out.print("_");
+            palavraOculta.add("_");
+        }
+        System.out.println("\n");
+
         leia.close();
     }
 }
