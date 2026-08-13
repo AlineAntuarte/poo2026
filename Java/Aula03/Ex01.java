88fe0412e0f45f56162a32766f424c95626a9456
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2026-08-12 22:27:56 -0300

    feat: Resolução dos Exercícios

--- Java/Aula03/Ex01.java
+++ Java/Aula03/Ex01.java
@@ -1,26 +1,70 @@
 package Java.Aula03;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 // Jogo da Forca
 public class Ex01 {
 
     public static void main(String[] args) {
+        System.out.println("== Jogo da Forca ==");
         Scanner leia = new Scanner(System.in);
 
-        boolean Trava = true;
         int tentativas = 5;
 
+        boolean Trava = true;
+
+        // Aqui teremos a vasta lista de palavras para advinhar
         String[] listaDePalavras = { "Antuarte" };
-        String[] palavraOculta = {};
+        // Este array no momento será testado com apenas uma palavra
+        List<String> palavraOculta = new ArrayList<>();
 
-        int letras = listaDePalavras[0].length();
+        // Cadeia de letras da palavra da vez
+        char[] letras = listaDePalavras[0].toCharArray();
 
-        for (int i = 0; i < letras; i++) {
-            System.out.print("_");
+        for (int i = 0; i < listaDePalavras[0].length(); i++) {
+            // Sabendo a quantidade de letras, sem posição, posso fazer a Palavra oculta no
+            // terminal com Underline
             palavraOculta.add("_");
         }
-        System.out.println("\n");
+        boolean acertou = false;
+
+        while (Trava) {
+
+            for (int i = 0; i < listaDePalavras[0].length(); i++) {
+                System.out.print(palavraOculta.get(i));
+            }
+
+            System.out.print("\n\nInsira uma Letra: ");
+            String userStringLetra = leia.nextLine();
+
+            // Varrer a palavra original e substituir no indice indicado
+            for (int i = 0; i < listaDePalavras[0].length(); i++) {
+                if (userStringLetra.equalsIgnoreCase(String.valueOf(letras[i]))) {
+                    palavraOculta.set(i, userStringLetra);
+                    acertou = true;
+                }
+            }
+
+            if (acertou) {
+                System.out.println("Acertou alguma coisa");
+                acertou = false;
+            } else {
+                tentativas--;
+                System.out.println("Não achou letra alguma");
+            }
+
+            System.out.println("\n");
+            if (!palavraOculta.contains("_")) {
+                Trava = false;
+                // Nenhuma letra oculta, jogo ENCERRA.
+            } else if (tentativas == 0) {
+                // Chances acabam, jogo encerra.
+            }
+            System.out.println("\n");
+
+        }
 
         leia.close();
     }
