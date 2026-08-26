fc9363e45d8d8d0dbb79b9626cbba4846edffdc3
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2026-08-25 22:15:43 -0300

    feat: Iniciando Aula 04

---
+++ Java/Aula04/Pessoa.java
@@ -0,0 +1,28 @@
+package Java.Aula04;
+
+import java.util.Scanner;
+
+// Criar Pessoa
+public class Pessoa {
+    // Class
+    public static void main(String[] args) {
+        // Main
+        Scanner leia = new Scanner(System.in);
+        leia.close();
+    }
+    // Funções
+}
+
+/*
+ * Exercício
+ * 
+ * Criar uma classe "Pessoa" com os
+ * seguintes atributos: Nome; Altura; Idade e Peso.
+ * 
+ * A classe também necessita dos seguintes comportamentos:
+ * calcularIMC - Calculo: peso / (altura²)
+ * ehMaiorDeIdade
+ * 
+ * Monte um sistema iterativo para cadastrar quatro pessoas em uma listaMostre
+ * quem é maior de idade e seu IMC.
+ */
