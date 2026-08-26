dbeee9b94fe5a54a1c0a212e61a610287df497b4
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2026-08-25 22:10:01 -0300

    feat: Iniciando Aula 04

--- Java/Aula04/Ex01.java
+++ Java/Aula04/Ex01.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 // Criar Pessoa
 public class Ex01 {
+    // Class
     public static void main(String[] args) {
+        // Main
         Scanner leia = new Scanner(System.in);
         leia.close();
     }
-
+    // Funções
 }
