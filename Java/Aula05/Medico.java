fe11b67ea4bc6be0e608421ef048d67e1ee1b4d1
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2026-09-15 21:17:31 -0300

    feat: Exercício Aula 15/09

--- Java/Aula05/Medico.java
+++ Java/Aula05/Medico.java
@@ -1,12 +1,12 @@
 package Java.Aula05;
 
-public class Engenheiro {
+public class Medico {
     private String cpf;
     private String nome;
     private String crea;
     private float salarioBruto;
 
-    public Engenheiro(String cpf, String nome, String crea, float salarioBruto) {
+    public Medico(String cpf, String nome, String crea, float salarioBruto) {
         this.cpf = cpf;
         this.nome = nome;
         this.crea = crea;
@@ -17,7 +17,7 @@ public class Engenheiro {
         return salarioBruto - (salarioBruto * desconto) + bonus;
     }
 
-    public void emitirLaudo() {
-        System.out.println("Emitindo laudo...");
+    public void emitirexame() {
+        System.out.println("Emitindo exame...");
     }
 }
---
+++ Java/Aula05/Sistema.java
@@ -0,0 +1,11 @@
+package Java.Aula05;
+
+import java.util.Scanner;
+
+public class Sistema {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        leia.close();
+    }
+}
